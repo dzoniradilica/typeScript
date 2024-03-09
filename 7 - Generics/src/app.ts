b26548c369080209910c66cbdc1ac49db78ef1fc
dzoniradilica <dzonidzonatan2@gmail.com>
2024-03-09 14:44:33 +0100

    #

--- 7 - Generics/src/app.ts
+++ 7 - Generics/src/app.ts
@@ -1,28 +1,35 @@
-// const names: Array<string> = ['dzoni']
+// const names: Array<string> = ['dzoni', 'nikola'];
 
 // const promise: Promise<string> = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('opic')
-//     }, 1000)
+//   setTimeout(() => {
+//     resolve('This is done')
+//   }, 2000)
+// })
+
+// promise.then(data => {
+//   data.split('')
 // })
 
 function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: 'nikola', hobbies: ['opic'] }, { age: 20 });
+const mergeObj = merge({ name: 'dzoni' }, { age: 20 });
 
-interface lengthy {
+interface Lenghty {
   length: number;
 }
 
-function countAndPrint<T extends lengthy>(element: T): [T, string] {
+function countAndPrint<T extends Lenghty>(element: T): [T, string] {
   let descriptionText = 'Got no value';
   if (element.length === 1) {
-    descriptionText = `Got 1 elements`;
+    descriptionText = 'Got 1 element';
   }
 
-  if (element.length > 1) descriptionText = `Got ${element.length} elements`;
+  if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+
   return [element, descriptionText];
 }
 
@@ -37,7 +44,7 @@ function extractAndConvert<T extends object, U extends keyof T>(
 
 extractAndConvert({ name: 'dzoni' }, 'name');
 
-class DataStorage<T extends string | number | boolean> {
+class DataStorage<T> {
   private data: T[] = [];
 
   addItem(item: T) {
@@ -54,28 +61,9 @@ class DataStorage<T extends string | number | boolean> {
 }
 
 const textStorage = new DataStorage<string>();
-textStorage.addItem('dzoni');
+textStorage.addItem('Dzoni');
+textStorage.addItem('nikola');
+textStorage.removeItem('Dzoni');
 console.log(textStorage.getItems());
 
 const numberStorage = new DataStorage<number>();
-
-interface CourseGoal {
-  title: string;
-  description: string;
-  complateUntil: Date;
-}
-
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.complateUntil = date;
-
-  return courseGoal as CourseGoal;
-}
-
-const names: Readonly<string[]> = ['max', 'dzoni'];
